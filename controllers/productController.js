93cdd5f5e8c73714a7c2d4fff2732e8d6487e12a
uraden <jfahriddinov97@gmail.com>
2023-09-13 23:08:32 +0500

    minor change

--- controllers/productController.js
+++ controllers/productController.js
@@ -8,11 +8,13 @@ const createProduct = async (req, res) => {
   const product = await Product.create(req.body);
   res.status(StatusCodes.CREATED).json({ product });
 };
+
 const getAllProducts = async (req, res) => {
   const products = await Product.find({});
 
   res.status(StatusCodes.OK).json({ products, count: products.length });
 };
+
 const getSingleProduct = async (req, res) => {
   const { id: productId } = req.params;
 
@@ -24,6 +26,7 @@ const getSingleProduct = async (req, res) => {
 
   res.status(StatusCodes.OK).json({ product });
 };
+
 const updateProduct = async (req, res) => {
   const { id: productId } = req.params;
 
@@ -38,6 +41,7 @@ const updateProduct = async (req, res) => {
 
   res.status(StatusCodes.OK).json({ product });
 };
+
 const deleteProduct = async (req, res) => {
   const { id: productId } = req.params;
 
@@ -50,6 +54,7 @@ const deleteProduct = async (req, res) => {
   await product.remove();
   res.status(StatusCodes.OK).json({ msg: 'Success! Product removed.' });
 };
+
 const uploadImage = async (req, res) => {
   if (!req.files) {
     throw new CustomError.BadRequestError('No File Uploaded');
